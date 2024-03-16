43b6d7e5f0a2b2577f4079e4199a8d2f8e762982
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-16 23:00:06 +0100

    Add func login and event listener for loginForm in app.js

--- client/app.js
+++ client/app.js
@@ -6,3 +6,19 @@ const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 
 let userName = '';
+
+const login = (event) => {
+    event.preventDefault();
+
+    const userNameValue = userNameInput.value.trim();
+    if(userNameValue !== ''){
+        userName = userNameValue;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    } else {
+        alert('User name can not be empty!');
+        return; 
+    }
+};
+
+loginForm.addEventListener('submit', login);
