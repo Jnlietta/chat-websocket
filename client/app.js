9b988a980c515e7fa3218c5f7d38664c955f568e
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-16 21:58:35 +0100

    Add variable userName in app.js

--- client/app.js
+++ client/app.js
@@ -4,3 +4,5 @@ const messagesList = document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
+
+let userName = '';
