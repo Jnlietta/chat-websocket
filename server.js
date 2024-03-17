14fe26e3eb5d5021a5f8786709e79069950b49b3
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-17 15:22:31 +0100

    Add object userLeft in listener disconnect use it in emiter userLeft and describe the code in server.js

--- server.js
+++ server.js
@@ -25,29 +25,51 @@ io.on('connection', (socket) => {
 
   //listener for join
   socket.on('join', (login) => {
+    //add to object login value id
     login.id = socket.id;
+
+    //add data of new user to array users
     users.push(login);
     console.log('New client on data base is named ' + login.author);
+
+    //create newUser object
     const newUser = {};
     newUser.author = 'Chat Bot';
     newUser.content = login.author + ' has joined the conversation!';
+
+    //emiter for other users that there is newUser
     socket.broadcast.emit('newUser', newUser);
   });
 
   // listener for message
   socket.on('message', (message) => {
     console.log('Oh, I\'ve got something from ' + socket.id);
+
+    //add data of message to messages array
     messages.push(message);
+
+    //emiter for other users to got a message
     socket.broadcast.emit('message', message);
   });
 
   //listener for disconnect
   socket.on('disconnect', () => { 
+    //find user that left chat in array users and remove it from array
     const user = users.find(user => user.id === socket.id);
     const indexOfUser = users.indexOf(user);
     users.splice(indexOfUser, 1);
+
     console.log('Oh, socket ' + socket.id + ' has left');
     console.log('User ' + user.author + ' has been deleted from data base.');
+
+    //create object userLeft
+    const userLeft = {};
+    userLeft.author = 'Chat Bot';
+    userLeft.content = user.author + ' has left the conversation... :(.';
+
+    //emiter for other users that user has left
+    socket.broadcast.emit('userLeft', userLeft);
   });
+
   console.log('I\'ve added a listener on message event \n');
 });
