0d556af76da91e4c9715bfaaff57fb7033bf6ccd
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-17 11:46:50 +0100

    Add middleware and endpoint to add diplay of app client in path '/' in server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const messages = [];
+
+app.use(express.static(path.join(__dirname, '/client'))); //middleware pozwalajacy na udostepnianie plików przez server
+
+app.get('*', (req, res) => { //endpoint dodajacy wyswietlanie aplikacji z /client pod "/"
+    res.sendFile(path.join(__dirname, '/client/index.html')); // wylapuje wszystkie linki i renderuje plik /client/index.html
+  });
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
